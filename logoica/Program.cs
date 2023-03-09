b02a38eae6d109e79c659f590d290af1dc7a8d5d
2022103030038 <rodrigijanine2@gmail.com>
2023-03-09 14:23:36 -0400

    Adicionar arquivos de projeto.

---
+++ logoica/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logoica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo!");
+            Console.ReadKey();
+        }
+    }
+}
